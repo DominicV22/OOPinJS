b6fa2386cda89dc633af74ce24cafa26ea3ca100
Dominic Roberts-Evans <dominicrobertsevans@yahoo.com>
2017-10-26 13:38:38 -0400

    egreger

--- sketch.js
+++ sketch.js
@@ -4,23 +4,28 @@ function setup() {
 	createCanvas(600, 400); 
 	for(var i = 0; i < 10; i++) {
 		var x = random(width);
-		var y = random(length);
+		var y = random(height);
 		var r = random(10, 40);
-		bubbles[i] = new Bubble(x, y, r);
+		var b = new Bubble(x, y, r);
 		bubbles.push(b);
 	}
 }
 function draw() { 
 	background(0); 
 	for (var i = 0; i < bubbles.length; i++) {
+		if(bubbles[i].contains(mouseX, mouseY)) {
+			bubbles[i].changeColor();
+		}
 		bubbles[i].move();
 		bubbles[i].show();
 	}
 }
+
 function mousepressed(){
 	var r = random(10,50);
-	var b = new Bubble(mouseX, mouseY, r)
+	var b = new Bubble(mouseX, mouseY, r);
 }
+
 class Bubble {
 	constructor(x, y, r) {
 		this.x = x;
@@ -28,10 +33,16 @@ class Bubble {
 		this.r = r;
 		this.brightness = 0;
 	}
-	clicked (px, py){
+	changeColor() {
+		this.brightness = 255;
+	}
+	
+	contains(px, py){
 		var d = dist(px, py, this.x, this.y);
-		if (d this.r){
-			this brightness = 255;
+		if (d < this.r){
+			return true;
+		} else {
+			return false;
 		}
 	}
 	move() {
